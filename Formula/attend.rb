9df3d77783f2fad89f37f1fa76127d80fd893d01
Doug Turnbull <softwaredoug@gmail.com>
2023-07-26 09:58:29 -0400

    Upgrade to 0.0.8

--- Formula/attend.rb
+++ Formula/attend.rb
@@ -1,8 +1,8 @@
 class Attend < Formula
   desc "App that gives you high score for avoiding task switching"
-  version "0.0.7"
+  version "0.0.8"
   homepage "https://github.com/softwaredoug/attend"
-  url "https://github.com/softwaredoug/attend.git", :tag => "v0.0.7"
+  url "https://github.com/softwaredoug/attend.git", :tag => "v0.0.8"
   sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
 
   def install
