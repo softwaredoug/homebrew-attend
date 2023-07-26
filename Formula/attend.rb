80eaa3156c8abbbc1faa459d856edcccf7354e20
Doug Turnbull <softwaredoug@gmail.com>
2023-07-26 08:32:32 -0400

    Initial release - 0.0.3

---
+++ Formula/attend.rb
@@ -0,0 +1,28 @@
+class Attend < Formula
+  desc "App that gives you high score for avoiding task switching"
+  version "0.0.3"
+  homepage "https://github.com/softwaredoug/attend"
+  url "https://github.com/softwaredoug/attend.git", :tag => "v0.0.3"
+  sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+
+  def install
+    mv "attend.sh", "attend"
+
+    # Move supporting scripts using libexec
+    libexec.install "attend"
+
+    # Supporting scripts include 
+    #   idle.sh
+    #   focusedapp.scpt
+    #   log.sh
+    #   tada.mp3
+    libexec.install "idle.sh"
+    libexec.install "focusedapp.scpt"
+    libexec.install "log.sh"
+    libexec.install "tada.mp3"
+
+    # Create symlink to attend.sh
+    bin.write_exec_script (libexec/"attend")
+  end
+
+end
